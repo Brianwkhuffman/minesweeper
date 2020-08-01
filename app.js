3f1c56302c202ae1c3bfadc66b963d2871ebc3b6
bri <brianwkhuffman@gmail.com>
2020-07-31 21:47:38 -1000

    makes grid box

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let width = 10;
+    
+
+//create grid
+function createBoard() {
+    // for (let i=0;i<width*width;i++){
+
+
+    // }
+}
+
+})

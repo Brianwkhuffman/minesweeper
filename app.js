eccf07d68e1a9ac66edadfb8bd3844cb1ff528a0
bri <brianwkhuffman@gmail.com>
2020-07-31 23:19:30 -1000

    adds grids & arrays

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     let width = 10;
-    
+    let bombAmount = 20;
+    let squaresArray = [];
 
-//create grid
+//creates grid
 function createBoard() {
-    // for (let i=0;i<width*width;i++){
+    //get shuffled game array with random bomb placement(?)
+    const bombsArray = Array(bombAmount).fill('bomb');
+    const emptyArray = Array(width*width-bombAmount).fill('valid');
 
+    const gameArray = emptyArray.concat(bombsArray)
+    console.log(gameArray)
 
-    // }
+    for (let i=0;i<width*width;i++){
+        const square = document.createElement('div');
+        square.setAttribute('id', i);
+        grid.appendChild(square);
+        squaresArray.push(square);
+    }
 }
+createBoard();
 
 })

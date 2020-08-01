368361b7ccd43f182f9c30f7288c9a2d1b7fce5a
bri <brianwkhuffman@gmail.com>
2020-07-31 23:42:23 -1000

    created array to shuffle bombs

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ function createBoard() {
     //get shuffled game array with random bomb placement(?)
     const bombsArray = Array(bombAmount).fill('bomb');
     const emptyArray = Array(width*width-bombAmount).fill('valid');
-
-    const gameArray = emptyArray.concat(bombsArray)
+    const gameArray = emptyArray.concat(bombsArray);
     console.log(gameArray)
+    const shuffledArray = gameArray.sort(() => Math.random()-0.5);
+    console.log(shuffledArray)
 
     for (let i=0;i<width*width;i++){
         const square = document.createElement('div');

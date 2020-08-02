d022247d6aeb976002eed6aa3d3bedd6aaa69d6f
bri <brianwkhuffman@gmail.com>
2020-08-02 00:51:10 -1000

    adds bombs to grid

--- app.js
+++ app.js
@@ -10,13 +10,12 @@ function createBoard() {
     const bombsArray = Array(bombAmount).fill('bomb');
     const emptyArray = Array(width*width-bombAmount).fill('valid');
     const gameArray = emptyArray.concat(bombsArray);
-    console.log(gameArray)
     const shuffledArray = gameArray.sort(() => Math.random()-0.5);
-    console.log(shuffledArray)
 
     for (let i=0;i<width*width;i++){
         const square = document.createElement('div');
         square.setAttribute('id', i);
+        square.classList.add(shuffledArray[i]);
         grid.appendChild(square);
         squaresArray.push(square);
     }

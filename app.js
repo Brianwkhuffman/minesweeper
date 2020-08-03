c51b32753adbc8c228a46871b68570f1a6e593a3
bri <brianwkhuffman@gmail.com>
2020-08-03 12:48:01 -1000

    removes some ;

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid');
-    let width = 10;
-    let bombAmount = 20;
-    let squaresArray = [];
+    const grid = document.querySelector('.grid')
+    let width = 10
+    let bombAmount = 20
+    let squaresArray = []
 
 //creates grid
 function createBoard() {
     //get shuffled game array with random bomb placement
-    const bombsArray = Array(bombAmount).fill('bomb');
-    const emptyArray = Array(width*width-bombAmount).fill('valid');
-    const gameArray = emptyArray.concat(bombsArray);
-    const shuffledArray = gameArray.sort(() => Math.random()-0.5);
+    const bombsArray = Array(bombAmount).fill('bomb')
+    const emptyArray = Array(width*width-bombAmount).fill('valid')
+    const gameArray = emptyArray.concat(bombsArray)
+    const shuffledArray = gameArray.sort(() => Math.random()-0.5)
 
     for (let i=0;i<width*width;i++){
-        const square = document.createElement('div');
-        square.setAttribute('id', i);
-        square.classList.add(shuffledArray[i]);
-        grid.appendChild(square);
-        squaresArray.push(square);
+        const square = document.createElement('div')
+        square.setAttribute('id', i)
+        square.classList.add(shuffledArray[i])
+        grid.appendChild(square)
+        squaresArray.push(square)
 
-        //normal click
+        //adding click to squares
         square.addEventListener('click', function(e) {
             click(square)
         })
@@ -27,9 +27,9 @@ function createBoard() {
 
     //add numbers to grid
     for (let i=0;i<squaresArray.length;i++){
-        let total = 0;
-        const isLeftEdge = (i % width === 0);
-        const isRightEdge = (i === width - 1);
+        let total = 0
+        const isLeftEdge = (i % width === 0)
+        const isRightEdge = (i === width - 1)
         //logic for square numbers
         if (squaresArray[i].classList.contains('valid')){
             if (i > 0 && !isLeftEdge && squaresArray[i-1].classList.contains('bomb')) total++
@@ -40,7 +40,7 @@ function createBoard() {
             if (i < 90 && !isLeftEdge && squaresArray[i-1+width].classList.contains('bomb')) total++
             if (i < 88 && !isRightEdge && squaresArray[i+1+width].classList.contains('bomb')) total++
             if (i < 89 && squaresArray[i+width].classList.contains('bomb')) total++
-            squaresArray[i].setAttribute('data', total);
+            squaresArray[i].setAttribute('data', total)
             console.log(squaresArray[i]) 
     }
 }
@@ -53,7 +53,7 @@ function createBoard() {
 
 
 
-createBoard();
+createBoard()
 
 //click on square actions
 function click(square) {
